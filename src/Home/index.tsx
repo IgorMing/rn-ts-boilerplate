ff268e5728e90da9b9494999c121005adf83f817
Igor Mesquita <igormesquita@Igors-MacBook-Pro.local>
2020-04-09 02:51:13 -0300

    add react-navigation

---
+++ src/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, SafeAreaView } from 'react-native';
+
+const Home: React.FC = () => {
+  return (
+    <SafeAreaView>
+      <Text>Home Screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
